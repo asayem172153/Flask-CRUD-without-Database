65f9244af3190bb2ac79f4f263a5491271697c11
Abu Sayem <abusayem379@gmail.com>
2023-12-14 00:15:44 +0600

    Getting ready for Crud

--- app.py
+++ app.py
@@ -1,4 +1,33 @@
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)
 
-print("Hi flask")
+data = [
+    {
+        'id' : 1,
+        'model_name' : 'KNN',
+    },
+    {
+        'id' : 2,
+        'model_name' : 'RNN',
+    },
+    {
+        'id' : 3,
+        'model_name' : 'Random Forest',
+    },
+    {
+        'id' : 4,
+        'model_name' : 'LR',
+    },
+    {
+        'id' : 5,
+        'model_name' : 'Logistic R',
+    },
+]
+
+@app.route('/home')
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+if __name__ == "__main__":
+    app.run()
